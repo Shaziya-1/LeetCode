5d24c9efb42762fb4d5d75ed7ec0cf72af96f410
Shaziya-1 <97125582+Shaziya-1@users.noreply.github.com>
2026-08-20 23:55:31 +0530

    Time: 1135 ms (11.04%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-delete p1 from Person p1 inner join Person p2  
+delete p1 from Person p1 inner join Person p2 
 on p1.email=p2.email and p1.id>p2.id ;
 
 -- delete p1 from Person p1 , Person p2
