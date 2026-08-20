2bcd4b6267026a14e61ea10061d9b20575744bee
Shaziya-1 <97125582+Shaziya-1@users.noreply.github.com>
2026-08-21 00:45:01 +0530

    Time: 381 ms (15.06%), Space: 0B (100%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,14 +1,8 @@
-# Write your MySQL query statement below
-select x ,y,z ,
-case 
-when x+y> z 
- and x+z>y           
-and y+z>x             
+select x ,y,z,
+case when x+y>z
+and x+z>y
+and y+z>x
 then 'Yes'
 else 'No'
 end as triangle
-from triangle;
-
---  13+15>30 - 28>30 no
---  10+15> 20 - 25>20 yes
--- Kyunki koi bhi ek side baaki dono sides ke sum se badi ya equal hui, triangle nahi banega.
+from triangle
