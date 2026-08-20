13ab9019871f413c2ba0d7b402d06016ef847943
Shaziya-1 <97125582+Shaziya-1@users.noreply.github.com>
2026-08-21 00:32:45 +0530

    Time: 645 ms (14.62%), Space: 0B (100%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -1,6 +1,23 @@
+# Write your MySQL query statement below
 select id ,
-case when p_id is null then 'Root'
- when id in(select P_id from Tree) then 'Inner'
- else 'Leaf'
- end as type
- from Tree ;
+case 
+when p_id is null then 'Root'
+when id in(select p_id from tree ) then 'Inner'
+else 'Leaf'
+end as type
+from tree; 
+
+-- p_id IS NULL
+-- → Root
+
+-- id parent list mein hai
+-- → Inner
+
+-- otherwise
+-- → Leaf  
+
+-- 1 → parent nahi → Root
+-- 2 → child 4,5 hain → Inner
+-- 3 → koi child nahi → Leaf
+-- 4 → koi child nahi → Leaf
+-- 5 → koi child nahi → Leaf
