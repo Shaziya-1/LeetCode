480ecdfd41547f7f4dc9f912d3b3b694609e5583
Shaziya-1 <97125582+Shaziya-1@users.noreply.github.com>
2026-08-21 00:11:25 +0530

    Time: 507 ms (62.31%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 select c.name from Customer c left join Customer c2
-on c.id=c2.id where c.referee_id!=2 or c.referee_id is null ;
+on c.id=c2.id where c2.referee_id !=2 or c2.referee_id is null;
