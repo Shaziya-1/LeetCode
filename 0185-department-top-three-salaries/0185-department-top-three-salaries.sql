50a6857ce669f7d1e95a96081e49b008a5bdbdd8
Shaziya-1 <97125582+Shaziya-1@users.noreply.github.com>
2026-08-21 15:16:00 +0530

    Time: 2796 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 select e1.name as Employee ,d.name as Department , e1.salary as Salary
 from Employee e1 inner join Department d on e1.departmentID=d.id
-where  3 >( select count(distinct  e2.Salary) from Employee  e2 where
- e2.departmentID=e1.departmentId and e2.Salary>e1.salary);
+where  3 >( select count(distinct e2.Salary) from Employee  e2 where
+ e1.departmentId=e2.departmentID and e2.Salary>e1.salary);
